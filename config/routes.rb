5f0642491c9150d73ef736d450b2d2b14ac531a3
Gaulthier Remusat <gaulthier.remusat@kedgebs.com>
2024-06-11 14:25:07 +0200

    ajout des deux variables txt sur users, et photo de profil. Setup cloudinary potetiellement bancal

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :witnesses, only: %i[show new create edit update destroy]
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
