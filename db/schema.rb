88101a8b93cff76a3835bbbc5e07c868beaa8a6e
maeva belleau <maevabf@hotmail.com>
2024-06-12 14:22:44 +0200

    footer + dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_11_145849) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_12_094515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_11_145849) do
     t.index ["witness_id"], name: "index_bookings_on_witness_id"
   end
 
+  create_table "dashboards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -74,6 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_11_145849) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_witnesses_on_user_id"
   end
 
