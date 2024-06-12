9c7760635665f2eb4e18cfca18faf5c5e4ffe762
maeva belleau <maevabf@hotmail.com>
2024-06-12 15:04:30 +0200

    dashboard en cours

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2024_06_12_094515) do
-=======
 ActiveRecord::Schema[7.1].define(version: 2024_06_12_122002) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,7 +80,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_12_122002) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "description"
     t.bigint "user_id", null: false
     t.text "description"
     t.string "gender"
