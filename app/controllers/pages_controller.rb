171f0729bc0a0e100e8058abe90131f367bf149e
loana bronico <loana.bronico@kedgebs.com>
2024-06-14 14:07:30 +0200

    back de la search bar + front du bouton voir plus

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home show]
 
   def home
+    @witness = Witness.new
+
     @witnesses = Witness.all
+    if params[:witness].present?
+      if params[:witness][:name].present? || params[:witness][:gender].present? || params[:witness][:price_per_day].present?
+        @witnesses = Witness.search_name_gender_and_price_per_day("#{params[:witness][:name]} #{params[:witness][:gender]} #{params[:witness][:price_per_day]}")
+      end
+    end
   end
 
   def dashboard
